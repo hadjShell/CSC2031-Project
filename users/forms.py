42ca7ad33513da41c30e49352058d5c2c8aa854f
Jiayuan Zhang <c0057833@ncl.ac.uk>
2021-10-21 23:34:42 +0100

    Validate password

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
+import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required, Email, Length, ValidationError
 
 
 class RegisterForm(FlaskForm):
@@ -9,7 +10,15 @@ class RegisterForm(FlaskForm):
     firstname = StringField(validators=[Required()])
     lastname = StringField(validators=[Required()])
     phone = StringField(validators=[Required()])
-    password = PasswordField(validators=[Required()])
+    # Password must be between 6 and 12 characters in length
+    password = PasswordField(validators=[Required(),
+                                         Length(min=6, max=12, message='Password must be between 6 and 12 characters in length.')])
     confirm_password = PasswordField(validators=[Required()])
     pin_key = StringField(validators=[Required()])
     submit = SubmitField()
+
+    # Password must contain at least 1 digit, 1 lowercase, 1 uppercase and 1 special character
+    def validate_password(self, password):
+        p = re.compile(r'(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^A-Za-z0-9])')
+        if not p.match(self.password.data):
+            raise ValidationError("Password must contain at least 1 digit, 1 lowercase, 1 uppercase and 1 special character.")
